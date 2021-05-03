20c7d059d352ce5fc58d677a6a1ac7d372822cbc
RaviP14 <ravi.patel14@outlook.com>
2021-05-03 16:43:24 +0100

    16 x 16 grid made - need to make rows/columns variable.

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const container = document.querySelector('.container');
 
-let gridType = (16 * 16);
+function makeGrid(rows, columns) {
 
-for (let i = 0; i < gridType; i++) {
-    let gridDiv = document.createElement('div');
-    gridDiv.className = 'gridDiv';
-    gridDiv.style.border = 'medium solid #000000'
-    gridDiv.style.width = '0.5em';
-    gridDiv.style.height = '0.5em';
-    container.appendChild(gridDiv);
+    for (let i = 0; i < (rows * columns); i++) {
+        let gridDiv = document.createElement('div');
+        gridDiv.className = 'gridDiv';
+        gridDiv.style.border = 'thin solid #000000'
+        gridDiv.style.width = '2em';
+        gridDiv.style.height = '2em';
+        container.appendChild(gridDiv);
+    }
 }
+
+makeGrid(16,16)
+//doesn't work correctly
+const hover = document.querySelector('.gridDiv');
+hover.addEventListener('hover', () => {
+    gridDiv.backgroundColor = 'black';
+});
